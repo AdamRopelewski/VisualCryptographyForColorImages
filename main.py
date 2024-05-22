c722395888010f691b81b4a8c529fcc249802c7a
AdamRopelewski <adam.ropelewski.kontakt@gmail.com>
2024-05-23 00:03:22 +0200

    using rgb to store h sv also encoding it

--- main.py
+++ main.py
@@ -1,45 +1,37 @@
 import cv2
-from tiles import get_tile, get_random_tile
+from tiles import get_random_tile, colors
 
 
-def modify_pixels_brightness(image, row, col, new_brightness):
-    """
-    Modifies the pixels brightness value at the specified row and column in the given HSV image.
-    """
-    pixel = image[row, col]
-    pixel[2] = new_brightness
-    image[row, col] = pixel
-    return image
+def is_pixel_is_bright(pixel):
+    return pixel[2] > 127
 
 
-def modify_pixels_saturation(image, row, col, new_saturation):
-    """
-    Modifies the pixels saturation value at the specified row and column in the given HSV image.
-    """
-    pixel = image[row, col]
-    pixel[1] = new_saturation
-    image[row, col] = pixel
-    return image
-
-
-def modify_pixels_hue(image, row, col, new_hue):
-    """
-    Modifies the pixels hue value at the specified row and column in the given HSV image.
-    """
-    pixel = image[row, col]
-    pixel[0] = new_hue % 179
-    image[row, col] = pixel
-    return image
-
-
-def check_if_bright_pixel(image_hsv, row, col):
-    """
-    Checks if the pixel at the specified row and column in the given HSV image has a brightness value greater than 0.5.
-    """
+def get_nears_basic_color(image_hsv, row, col):
     pixel = image_hsv[row, col]
-    if pixel[2] > 127:
-        return True
-    return False
+
+    min_hue_distance = 1000.0
+    min_saturation_distance = 1000.0
+    min_brightness_distance = 1000.0
+    closest_color = None
+
+    for color in colors[1:-1]:
+        hue_distance = 0.0
+        saturation_distance = 0.0
+        brightness_distance = 0.0
+        hue_distance += abs(pixel[0] - color[0])
+        saturation_distance += abs(pixel[1] - color[1])
+        brightness_distance += abs(pixel[2] - color[2])
+        if (
+            hue_distance <= min_hue_distance
+            and saturation_distance <= min_saturation_distance
+            and brightness_distance <= min_brightness_distance
+        ):
+            closest_color = color
+            min_hue_distance = hue_distance
+            min_saturation_distance = saturation_distance
+            min_brightness_distance = brightness_distance
+
+    return list(closest_color)
 
 
 def resize_image(image, x):
@@ -53,61 +45,117 @@ def resize_image(image, x):
     return new_image
 
 
+def tile_invert(tile):
+    return [[not i for i in tile[0]], [not i for i in tile[1]]]
+
+
 def encode_images(image_hsv):
     # Create a new HSV image with 4x resolution
     encoded_hsv_image_1 = resize_image(image_hsv, 4)
     encoded_hsv_image_2 = resize_image(image_hsv, 4)
+    encoded_hsv_image_3 = resize_image(image_hsv, 4)
+    encoded_rgb_image_1 = cv2.cvtColor(encoded_hsv_image_1, cv2.COLOR_HSV2RGB)
+    encoded_rgb_image_1_2 = cv2.cvtColor(encoded_hsv_image_1, cv2.COLOR_HSV2RGB)
+    encoded_rgb_image_2 = cv2.cvtColor(encoded_hsv_image_2, cv2.COLOR_HSV2RGB)
+    encoded_rgb_image_2_2 = cv2.cvtColor(encoded_hsv_image_2, cv2.COLOR_HSV2RGB)
+    encoded_rgb_image_3 = cv2.cvtColor(encoded_hsv_image_3, cv2.COLOR_HSV2RGB)
+    encoded_rgb_image_3_2 = cv2.cvtColor(encoded_hsv_image_3, cv2.COLOR_HSV2RGB)
     for row in range(image_hsv.shape[0]):
         for col in range(image_hsv.shape[1]):
+            # nears_basic_color = get_nears_basic_color(image_hsv, row, col)
             tile = get_random_tile()
             for i in range(len(tile[0])):
                 for j in range(len(tile[1])):
-                    # Check if the pixel is bright
+                    hue = image_hsv[row][col][0] % 256
+                    saturation = image_hsv[row][col][1] % 256
+                    brightness = image_hsv[row][col][2] % 256
                     if tile[i][j] == 1:
-                        brightness = 0
+                        encoded_rgb_image_1[row * 2 + i][col * 2 + j] = [
+                            brightness,
+                            saturation,
+                            hue,
+                        ]
+                        encoded_rgb_image_2[row * 2 + i][col * 2 + j] = [
+                            saturation,
+                            saturation,
+                            saturation,
+                        ]
+                        encoded_rgb_image_3[row * 2 + i][col * 2 + j] = [
+                            brightness,
+                            brightness,
+                            brightness,
+                        ]
                     else:
-                        brightness = 255 // 2
-                    encoded_hsv_image_1 = modify_pixels_brightness(
-                        encoded_hsv_image_1, row * 2 + i, col * 2 + j, brightness
-                    )
-                    encoded_hsv_image_1 = modify_pixels_saturation(
-                        encoded_hsv_image_1, row * 2 + i, col * 2 + j, brightness
-                    )
-                    encoded_hsv_image_1 = modify_pixels_hue(
-                        encoded_hsv_image_1, row * 2 + i, col * 2 + j, brightness
-                    )
+                        encoded_rgb_image_1[row * 2 + i][col * 2 + j] = [0, 0, 0]
+                        encoded_rgb_image_2[row * 2 + i][col * 2 + j] = [0, 0, 0]
+                        encoded_rgb_image_3[row * 2 + i][col * 2 + j] = [0, 0, 0]
+                    if not is_pixel_is_bright(image_hsv[row][col]):
+                        tile = tile_invert(tile)
+                    if tile[i][j] == 1:
+                        encoded_rgb_image_1_2[row * 2 + i][col * 2 + j] = [
+                            brightness,
+                            saturation,
+                            hue,
+                        ]
+                        encoded_rgb_image_2_2[row * 2 + i][col * 2 + j] = [
+                            saturation,
+                            saturation,
+                            saturation,
+                        ]
+                        encoded_rgb_image_3_2[row * 2 + i][col * 2 + j] = [
+                            brightness,
+                            brightness,
+                            brightness,
+                        ]
+                    else:
+                        encoded_rgb_image_1_2[row * 2 + i][col * 2 + j] = [0, 0, 0]
+                        encoded_rgb_image_2_2[row * 2 + i][col * 2 + j] = [0, 0, 0]
+                        encoded_rgb_image_3_2[row * 2 + i][col * 2 + j] = [0, 0, 0]
 
-                    if not check_if_bright_pixel(image_hsv, row, col):
-                        brightness = 255 // 2 - brightness
-
-                    encoded_hsv_image_2 = modify_pixels_brightness(
-                        encoded_hsv_image_2, row * 2 + i, col * 2 + j, brightness
-                    )
-                    encoded_hsv_image_2 = modify_pixels_saturation(
-                        encoded_hsv_image_2, row * 2 + i, col * 2 + j, brightness
-                    )
-                    encoded_hsv_image_2 = modify_pixels_hue(
-                        encoded_hsv_image_2, row * 2 + i, col * 2 + j, brightness
-                    )
-
-    return encoded_hsv_image_1, encoded_hsv_image_2
+    return (
+        encoded_rgb_image_1,
+        encoded_rgb_image_1_2,
+        encoded_rgb_image_2,
+        encoded_rgb_image_2_2,
+        encoded_rgb_image_3,
+        encoded_rgb_image_3_2,
+    )
 
 
-image = cv2.imread("input/ImageToBeCoded.png")
+image = cv2.imread("input/ImageToBeCoded5.png")
 
-
+# image = cv2.imread("input/test.png")
 # Convert the image from RGB to HSV
 image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 
+(
+    encoded_rgb_image_1,
+    encoded_rgb_image_1_2,
+    encoded_rgb_image_2,
+    encoded_rgb_image_2_2,
+    encoded_rgb_image_3,
+    encoded_rgb_image_3_2,
+) = encode_images(image_hsv)
 
-encoded_hsv_image_1, encoded_hsv_image_2 = encode_images(image_hsv)
-encoded_rgb_image_1 = cv2.cvtColor(encoded_hsv_image_1, cv2.COLOR_HSV2RGB)
-encoded_rgb_image_2 = cv2.cvtColor(encoded_hsv_image_2, cv2.COLOR_HSV2RGB)
+
+# encoded_rgb_image_1 = cv2.cvtColor(encoded_hsv_image_1, cv2.COLOR_HSV2RGB)
+# encoded_rgb_image_2 = cv2.cvtColor(encoded_hsv_image_2, cv2.COLOR_HSV2RGB)
+# encoded_rgb_image_3 = cv2.cvtColor(encoded_hsv_image_3, cv2.COLOR_HSV2RGB)
+
 # Display the image
-cv2.imwrite("output/EncodedImage1.png", encoded_rgb_image_1)
-cv2.imwrite("output/EncodedImage2.png", encoded_rgb_image_2)
+
 cv2.imshow("Image", image)
 cv2.imshow("Encoded1", encoded_rgb_image_1)
 cv2.imshow("Encoded2", encoded_rgb_image_2)
+cv2.imshow("Encoded3", encoded_rgb_image_3)
+cv2.imshow("Encoded1_2", encoded_rgb_image_1_2)
+cv2.imshow("Encoded2_2", encoded_rgb_image_2_2)
+cv2.imshow("Encoded3_2", encoded_rgb_image_3_2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+cv2.imwrite("output/EncodedImage1.png", encoded_rgb_image_1)
+cv2.imwrite("output/EncodedImage2.png", encoded_rgb_image_2)
+cv2.imwrite("output/EncodedImage3.png", encoded_rgb_image_3)
+cv2.imwrite("output/EncodedImage1_2.png", encoded_rgb_image_1_2)
+cv2.imwrite("output/EncodedImage2_2.png", encoded_rgb_image_2_2)
+cv2.imwrite("output/EncodedImage3_2.png", encoded_rgb_image_3_2)
--- tiles.py
+++ tiles.py
@@ -1,10 +1,34 @@
 import numpy as np
 
 
-def get_random_tile():
-    n = len(colorfull_tiles)
-    return colorfull_tiles[np.random.randint(0, n)]
+# def get_random_tile():
+#     n = len(colorfull_tiles)
+#     return colorfull_tiles[np.random.randint(0, n)]
 
+
+def get_random_tile():
+    return tiles[np.random.randint(0, 16)]
+
+
+# Define the tiles
+tiles = [
+    [[1, 1], [1, 1]],  # Tile 1
+    [[1, 1], [1, 0]],  # Tile 2
+    [[1, 1], [0, 1]],  # Tile 3
+    [[1, 1], [0, 0]],  # Tile 4
+    [[1, 0], [1, 1]],  # Tile 5
+    [[1, 0], [1, 0]],  # Tile 6
+    [[1, 0], [0, 1]],  # Tile 7
+    [[1, 0], [0, 0]],  # Tile 8
+    [[0, 1], [1, 1]],  # Tile 9
+    [[0, 1], [1, 0]],  # Tile 10
+    [[0, 1], [0, 1]],  # Tile 11
+    [[0, 1], [0, 0]],  # Tile 12
+    [[0, 0], [1, 1]],  # Tile 13
+    [[0, 0], [1, 0]],  # Tile 14
+    [[0, 0], [0, 1]],  # Tile 15
+    [[0, 0], [0, 0]],  # Tile 16
+]
 
 # Define the tiles cv2 in hsv format
 black = (0, 0, 0)
@@ -12,13 +36,13 @@ blue = (0, 255, 255)
 red = (120, 255, 255)
 green = (60, 255, 255)
 white = (0, 0, 255)
-colors = [black, blue, red, green, white]
+colors = [black, red, green, blue, white]
 
-n = len(colors)
-colorfull_tiles = []
-for i in range(n):
-    for k in range(n):
-        for j in range(n):
-            for l in range(n):
-                colorfull_tiles.append([[colors[i], colors[k]], [colors[j], colors[l]]])
-print(colorfull_tiles)
+# n = len(colors)
+# colorfull_tiles = []
+# for i in range(n):
+#     for k in range(n):
+#         for j in range(n):
+#             for l in range(n):
+#                 colorfull_tiles.append([[colors[i], colors[k]], [colors[j], colors[l]]])
+# print(colorfull_tiles)
