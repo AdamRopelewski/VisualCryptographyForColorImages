bd4e6e2b5395d53222b9181dddbbb8527fd913ce
AdamRopelewski <adam.ropelewski.kontakt@gmail.com>
2024-05-22 20:50:16 +0200

    tiles now consist of 5 diff colors

--- tiles.py
+++ tiles.py
@@ -1,30 +1,24 @@
 import numpy as np
 
 
-def get_tile(tile_id):
-    return tiles[tile_id - 1]
-
-
 def get_random_tile():
-    return tiles[np.random.randint(0, 16)]
+    n = len(colorfull_tiles)
+    return colorfull_tiles[np.random.randint(0, n)]
 
 
-# Define the tiles
-tiles = [
-    np.array([[1, 1], [1, 1]]),  # Tile 1
-    np.array([[1, 1], [1, 0]]),  # Tile 2
-    np.array([[1, 1], [0, 1]]),  # Tile 3
-    np.array([[1, 1], [0, 0]]),  # Tile 4
-    np.array([[1, 0], [1, 1]]),  # Tile 5
-    np.array([[1, 0], [1, 0]]),  # Tile 6
-    np.array([[1, 0], [0, 1]]),  # Tile 7
-    np.array([[1, 0], [0, 0]]),  # Tile 8
-    np.array([[0, 1], [1, 1]]),  # Tile 9
-    np.array([[0, 1], [1, 0]]),  # Tile 10
-    np.array([[0, 1], [0, 1]]),  # Tile 11
-    np.array([[0, 1], [0, 0]]),  # Tile 12
-    np.array([[0, 0], [1, 1]]),  # Tile 13
-    np.array([[0, 0], [1, 0]]),  # Tile 14
-    np.array([[0, 0], [0, 1]]),  # Tile 15
-    np.array([[0, 0], [0, 0]]),  # Tile 16
-]
+# Define the tiles cv2 in hsv format
+black = (0, 0, 0)
+blue = (0, 255, 255)
+red = (120, 255, 255)
+green = (60, 255, 255)
+white = (0, 0, 255)
+colors = [black, blue, red, green, white]
+
+n = len(colors)
+colorfull_tiles = []
+for i in range(n):
+    for k in range(n):
+        for j in range(n):
+            for l in range(n):
+                colorfull_tiles.append([[colors[i], colors[k]], [colors[j], colors[l]]])
+print(colorfull_tiles)
