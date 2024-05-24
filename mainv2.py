0321782558f88a56f37543c2fde213a6d4aa1f05
AdamRopelewski <adam.ropelewski.kontakt@gmail.com>
2024-05-24 19:44:12 +0200

    code refactor: naming and saving added

--- mainv2.py
+++ mainv2.py
@@ -1,78 +1,7 @@
 from PIL import Image
+from tilesv2 import colors, colors_translated
+import numpy as np
 
-colors = {
-    'dark gray': (64, 64, 64),
-    'black': (0, 0, 0),
-    'red': (255, 0, 0),
-    'green': (0, 255, 0),
-    'blue': (0, 0, 255),
-    'light gray': (192, 192, 192),
-    'white': (255, 255, 255),
-    'cyan': (0, 255, 255),
-    'magenta': (255, 0, 255),
-    'yellow': (255, 255, 0)
-}
-
-colors_translated = {
-    'dark gray': [
-        [ (0, 0, 0), (0, 0, 0), (0, 0, 0) ],
-        [ (255, 0, 0), (0, 255, 0), (0, 0, 255) ],
-        [ (0, 0, 0), (0, 0, 0), (0, 0, 0) ]
-    ],
-    'black': [
-        [ (0, 0, 0), (0, 0, 0), (0, 0, 0) ],
-        [ (0, 0, 0), (0, 0, 0), (0, 0, 0) ],
-        [ (0, 0, 0), (0, 0, 0), (0, 0, 0) ]
-    ],
-    'red': [
-        [ (255, 0, 0), (0, 0, 0), (0, 0, 0) ],
-        [ (255, 0, 0), (0, 0, 0), (0, 0, 0) ],
-        [ (255, 0, 0), (0, 0, 0), (0, 0, 0) ]
-    ],
-    'green': [
-        [ (0, 0, 0), (0, 255, 0), (0, 0, 0) ],
-        [ (0, 0, 0), (0, 255, 0), (0, 0, 0) ],
-        [ (0, 0, 0), (0, 255, 0), (0, 0, 0) ]
-    ],
-    'blue': [
-        [ (0, 0, 0), (0, 0, 0), (0, 0, 255) ],
-        [ (0, 0, 0), (0, 0, 0), (0, 0, 255) ],
-        [ (0, 0, 0), (0, 0, 0), (0, 0, 255) ]
-    ],
-    'light gray': [
-        [ (255, 0, 0), (0, 255, 0), (0, 0, 255) ],
-        [ (0, 0, 0), (0, 0, 0), (0, 0, 0)  ],
-        [ (255, 0, 0), (0, 255, 0), (0, 0, 255) ]
-    ],
-    'white': [
-        [ (255, 0, 0), (0, 255, 0), (0, 0, 255) ],
-        [ (255, 0, 0), (0, 255, 0), (0, 0, 255) ],
-        [ (255, 0, 0), (0, 255, 0), (0, 0, 255) ]
-    ],
-    'cyan': [
-        [ (0, 0, 0), (0, 255, 0), (0, 0, 255) ],
-        [ (0, 0, 0), (0, 255, 0), (0, 0, 255) ],
-        [ (0, 0, 0), (0, 255, 0), (0, 0, 255) ]
-    ],
-    'magenta': [
-        [ (255, 0, 0), (0, 0, 0), (0, 0, 255) ],
-        [ (255, 0, 0), (0, 0, 0), (0, 0, 255) ],
-        [ (255, 0, 0), (0, 0, 0), (0, 0, 255) ]
-    ],
-    'yellow': [
-        [ (255, 0, 0), (0, 255, 0), (0, 0, 0) ],
-        [ (255, 0, 0), (0, 255, 0), (0, 0, 0) ],
-        [ (255, 0, 0), (0, 255, 0), (0, 0, 0) ]
-    ]
-}
-
-n1 = 512
-n2 = 332
-
-# img = Image.new('RGB', (n1, n2))
-
-# for x, color in enumerate(colors.values()):
-#     img.putpixel((x%n1, x//n1), color)
 
 def dither_image(image_path):
     palette = []
@@ -80,41 +9,21 @@ def dither_image(image_path):
         palette.extend(color)
 
     image = Image.open(image_path)
-    if image.mode != 'RGB':
-        image = image.convert('RGB')
+    if image.mode != "RGB":
+        image = image.convert("RGB")
 
     palette_image = Image.new("P", (1, 1))
     palette_image.putpalette(palette)
 
     dithered_image = image.quantize(palette=palette_image, dither=Image.FLOYDSTEINBERG)
 
-    safe_image = dithered_image.convert('RGB')
+    safe_image = dithered_image.convert("RGB")
 
     return safe_image
 
-image_path = 'input/ImageToBeCoded5.png'
-img = dither_image(image_path)
-img.show()
-
-img2 = Image.new('RGB', (n1*3, n2*3))
-
-for x in range(n1):
-    for y in range(n2):
-        pixel_color = img.getpixel((x, y))
-        for color_name, color_values in colors.items():
-            if color_values == pixel_color:
-                replacement_color = colors_translated[color_name]
-                for i in range(3):
-                    for j in range(3):
-                        img2.putpixel((x*3 + i, y*3 + j), replacement_color[j][i])
-                break
-
-img2.show()
-
-import numpy as np
 
 def random_tile(color=(255, 255, 255)):
-    tile = Image.new('RGB', (2, 2))
+    tile = Image.new("RGB", (2, 2))
     for i in range(2):
         for j in range(2):
             new_color = color
@@ -124,37 +33,95 @@ def random_tile(color=(255, 255, 255)):
             tile.putpixel((i, j), new_color)
     return tile
 
-img3 = Image.new('RGB', (n1*6, n2*6))
 
-for x in range(n1):
-    for y in range(n2):
+image_path = "input/ImageToBeCoded5.png"
+dithered_image = dither_image(image_path)
+dithered_image.save("output/dithered_image.png")
+dithered_image.show("dithered_image")
+
+width = dithered_image.width
+height = dithered_image.height
+
+x3_res_dithered_image = Image.new("RGB", (width * 3, height * 3))
+
+for x in range(width):
+    for y in range(height):
+        pixel_color = dithered_image.getpixel((x, y))
+        for color_name, color_values in colors.items():
+            if color_values == pixel_color:
+                replacement_color = colors_translated[color_name]
+                for i in range(3):
+                    for j in range(3):
+                        x3_res_dithered_image.putpixel(
+                            (x * 3 + i, y * 3 + j), replacement_color[j][i]
+                        )
+                break
+x3_res_dithered_image.save("output/x3_res_dithered_image.png")
+x3_res_dithered_image.show()
+
+
+encoded_image_1 = Image.new("RGB", (width * 6, height * 6))
+
+for x in range(width):
+    for y in range(height):
         for i in range(3):
             for j in range(3):
-                img3.paste(random_tile((255 if i==0 else 0, 255 if i==1 else 0, 255 if i==2 else 0)), (x*6 + i*2, y*6 + j*2))
+                encoded_image_1.paste(
+                    random_tile(
+                        (
+                            255 if i == 0 else 0,
+                            255 if i == 1 else 0,
+                            255 if i == 2 else 0,
+                        )
+                    ),
+                    (x * 6 + i * 2, y * 6 + j * 2),
+                )
+encoded_image_1.save("output/encoded_image_1.png")
+encoded_image_1.show()
 
-img3.show()
+encoded_image_2 = Image.new("RGB", (width * 6, height * 6))
 
-img4 = Image.new('RGB', (n1*6, n2*6))
-
-for x in range(n1*3):
-    for y in range(n2*3):
-        pixel_color = img2.getpixel((x, y))
+for x in range(width * 3):
+    for y in range(height * 3):
+        pixel_color = x3_res_dithered_image.getpixel((x, y))
         if pixel_color == (0, 0, 0):
-            tile = Image.new('RGB', (2, 2))
+            tile = Image.new("RGB", (2, 2))
             for i in range(2):
                 for j in range(2):
-                    tile.putpixel((i, j), tuple(np.array(img3.getpixel((x*2 + i, y*2 + j))) ^ np.array((255 if x%3==0 else 0, 255 if x%3==1 else 0, 255 if x%3==2 else 0))))
-            img4.paste(tile, (x*2, y*2))
+                    tile.putpixel(
+                        (i, j),
+                        tuple(
+                            np.array(encoded_image_1.getpixel((x * 2 + i, y * 2 + j)))
+                            ^ np.array(
+                                (
+                                    255 if x % 3 == 0 else 0,
+                                    255 if x % 3 == 1 else 0,
+                                    255 if x % 3 == 2 else 0,
+                                )
+                            )
+                        ),
+                    )
+            encoded_image_2.paste(tile, (x * 2, y * 2))
         else:
-            img4.paste(random_tile((255 if x%3==0 else 0, 255 if x%3==1 else 0, 255 if x%3==2 else 0)), (x*2, y*2))
+            encoded_image_2.paste(
+                random_tile(
+                    (
+                        255 if x % 3 == 0 else 0,
+                        255 if x % 3 == 1 else 0,
+                        255 if x % 3 == 2 else 0,
+                    )
+                ),
+                (x * 2, y * 2),
+            )
+encoded_image_2.save("output/encoded_image_2.png")
+encoded_image_2.show()
 
-img4.show()
-
-img5 = Image.new('RGB', (n1*6, n2*6))
-for x in range(n1*6):
-    for y in range(n2*6):
-        pixel_color1 = img3.getpixel((x, y))
-        pixel_color2 = img4.getpixel((x, y))
+decodedImage = Image.new("RGB", (width * 6, height * 6))
+for x in range(width * 6):
+    for y in range(height * 6):
+        pixel_color1 = encoded_image_1.getpixel((x, y))
+        pixel_color2 = encoded_image_2.getpixel((x, y))
         and_color = tuple(np.array(pixel_color1) & np.array(pixel_color2))
-        img5.putpixel((x, y), and_color)
-img5.show()
+        decodedImage.putpixel((x, y), and_color)
+decodedImage.save("output/decoded_image.png")
+decodedImage.show()
